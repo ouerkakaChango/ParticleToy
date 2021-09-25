1d91be19c9ffe06b5fec5471613def21eb5078a3
ouerkakaChango <2293933523@qq.com>
2021-09-25 16:42:44 +0800

    整理

---
+++ Backup/molecule.cpp
@@ -0,0 +1,124 @@
+// ParticleToy.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#ifdef MOLECULE
+
+#include "pch.h"
+#include <iostream>
+
+#include "../include/Base/Spaces/Spaces.h"
+
+//### 应用
+//ParticleToy第三期应用：分子键（冰融化，刚体破碎，柔体...）
+//1.让粒子和地面实现带半径碰撞
+//2.两个粒子，在平地都受重力和万有引力影响，看效果（应该是互相吸到一起（带碰撞））
+//3.引入restPosition，让粒子收到基于偏移restPos的斥力，看是否能实现类似分子键的效果，让两个粒子能
+//保持一定距离
+//1.1 重写胶囊与三角面相交判定
+//4.现在比较hardcode,等会想想怎么把这个算restForce的属性抽象出来
+//5.relation换成基于restPos的再看一下效果
+//6.基于新的restPos force，测试立方体晶格稳定性，并思考是否让受外力太强的/距离太远的两原子的分子键断裂，以模拟破碎/融化？
+//7.??? 注意pnt在复制的时候outer的复制，现在先没管
+
+int main()
+{
+	MinkowskiSpace* world = new MinkowskiSpace;
+	world->SetFrameSettings(361, 0.0166666);
+	MinkowskiSpaceR* op = (MinkowskiSpaceR*)world->r[0];
+	op->SetGravity(P(0.0f, -9.80665, 0.0f));
+
+	//Pnt atom1(P(0.0f,1.0f,0.0f));
+	//atom1.name = "atom1";
+	//atom1.rule = "Molecule";
+	//atom1.SetSphereOuter(1.0f);
+	//atom1.relations += new RestPosRelation(1, P(4, 0, 0),20);
+	//atom1.relations += new RestPosRelation(2, P(0, 4, 0),100);
+	//op->PutPnt(atom1);
+	//
+	//Pnt atom2(P(4.0f, 1.0f, 0.0f));
+	//atom2.name = "atom2";
+	//atom2.rule = "Molecule";
+	//atom2.SetSphereOuter(1.0f);
+	//atom2.relations += new RestPosRelation(0, P(-4, 0, 0),20);
+	//op->PutPnt(atom2);
+	//
+	//Pnt atom3(P(0.0f, 5.0f, 0.0f));
+	//atom3.name = "atom3";
+	//atom3.rule = "Molecule";
+	//atom3.SetSphereOuter(1.0f);
+	//atom3.relations += new RestPosRelation(0, P(0, -4, 0), 100);
+	//op->PutPnt(atom3);
+	//
+	//Pnt atom4(P(4.0f, 5.0f, 0.0f));
+	//atom4.name = "atom4";
+	//atom4.rule = "Molecule";
+	//atom4.SetSphereOuter(1.0f);
+	//atom4.relations += new RestPosRelation(1, P(0, -4, 0), 100);
+	//op->PutPnt(atom4);
+
+	Grid* cellGrid = new Grid;
+	Pnt templatePnt;
+	templatePnt.rule = "Molecule";
+	templatePnt.SetSphereOuter(1.0f);
+	cellGrid->SetGrid3DSettings<Pnt>(1, 1, 1, 4.0f, templatePnt);
+	auto& grid3d = Cast<Grid3DI<Pnt>*>(cellGrid->i[0])->grid;
+	auto func = [](Pnt& pnt, P pntPos, int pntInx)
+	{
+		pnt.pos = pntPos + P(0, 1, 0);
+		pnt.name = str("atom");
+		pnt.name += pntInx;
+	};
+	grid3d.DoByPos(func);
+	auto func2 = [](Pnt& pnt, P pntPos, int pntInx)
+	{
+		std::cout << pnt.name << " " << pnt.rule << " " << pnt.pos.ToStr() << std::endl;
+	};
+	grid3d.DoByPos(func2);
+
+	auto relationFunc = [](Pnt& pnt, P dir, int otherInx)
+	{
+		if (dir.y != 0)
+		{
+			pnt.relations += new RestPosRelation(otherInx, 4 * dir, 20);
+		}
+		else
+		{
+			pnt.relations += new RestPosRelation(otherInx, 4 * dir, 10);
+		}
+	};
+	//对每个原子周围设置relation
+	grid3d.DoByNearest(relationFunc);
+
+	auto putPntFunc = [&](Pnt& pnt)
+	{
+		op->PutPnt(pnt);
+	};
+	grid3d.DoByInx(putPntFunc);
+
+	Grid* terrain = new Grid;
+	terrain->SetGridSettings<float>(2, 30.0f);
+	arr<Tri> triArr;
+	GridR* op2 = (GridR*)terrain->r[0];
+
+	//op2->DebugOutput("C:/HoudiniProjects/PToyScene/atomPlane.txt");
+
+	op2->TerrainToTri(triArr);
+	op->PutTri("terrain", triArr, "CollisionProp");
+
+	auto f1 = [&](TickInfo& info)
+	{
+		PhysicSolver* physic = info.physic;
+		auto F = info.F;
+		float nowRate = pow(1 - F / 180.0f, 1);
+		physic->RelationRate = nowRate;
+	};
+	op->AddBeforeTickFunc(f1);
+
+	op->Evolve(0);
+	//op->DebugSay();
+	op->OutputPntTrajTxt("C:/HoudiniProjects/PToyScene/golfBall.txt");
+
+
+	return 0;
+}
+
+#endif
---
+++ Backup/old_FFTocean.cpp
@@ -0,0 +1,105 @@
+﻿// ParticleToy.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#ifdef OLD_FFTOCEAN
+#include "pch.h"
+#include <iostream>
+
+#include "Render/Material.h"
+#include "rayTraceRender/rayTraceWorld.h"
+#include "rayTraceRender/rayTraceScreen.h"
+#include "Spaces/Grid.h"
+
+//### 应用
+//ParticleToy第五期应用：FFT海面
+
+//https://www.slideshare.net/Codemotion/an-introduction-to-realistic-ocean-rendering-through-fft-fabio-suriano-codemotion-rome-2017
+//1.可视化 Phillips spectrum
+
+//extra:优化
+//1.为了优化大量静态物件的sdf过程，以可视化静态Phillips spectrum为例：
+//确定好grid的长宽后，默认Box为一个平均高度h，然后将整个BounceSpace grid化。
+//每一个grid存储的是sdf-min的ObjInx。
+//在以后每次sdf时候，如果最近的两个ObjInx是一样的，那么直接O(1)找到min-sdf，而不是O(n)for循环去找minSDF。
+//如果不一样，那么两者比较取最小。
+//如果grid分辨率合理，是能没有瑕疵的。细分太大显然会出问题。
+
+int main()
+{
+	str pyWorkPath = "C:\\Users\\hasee\\source\\repos\\ParticleToy\\PythonWorkSpace";
+	bool pbrMode = false;
+	rayTraceWorld* world = new rayTraceWorld;
+	world->SetTraceSettings(1, rayTraceMode_SDFSphere, rayTraceBounceMode_cheap, rayTraceMaterialMode_BlinnPhong);
+	{
+		//world->SetOptimizeMode(rayTraceOptimizeMode_PerTask);
+		//auto opt = Cast<rayTraceOptimizePolicy_PerTask*>(world->optimizePolicy);
+		//opt->rayPerTask = 1080*6;
+	}
+	rayTraceWorldR* op = (rayTraceWorldR*)world->r[0];
+
+	Grid oceanGrid;
+	P2 edge(8, 5);
+	oceanGrid.SetGridSettings<Box*>(edge.x, edge.y, 0.5, nullptr);
+	auto& grid = Cast<GridI<Box*>*>(oceanGrid.i[0])->grid;
+
+	//###
+	double g = 9.8;
+	P2 windDir(1, 1);
+	windDir = safeNorm(windDir);
+	P2 L = edge * 0.5;
+	double waveAmplitude=0.3;
+
+	auto func_Ph = [&](P2 kvec)
+	{
+		double k = len(kvec);
+		return waveAmplitude * exp(-1 / pow(k*len(L), 2)) / pow(k, 4) * pow(dot(kvec, windDir), 2);
+	};
+
+	auto func_h0 = [&](double Ph)
+	{
+		double ksi1 = rand_norm(0, 1);
+		double ksi2 = rand_norm(0, 1);
+		return 1 / sqrt(2) * cplx(ksi1, ksi2) * sqrt(Ph);
+	};
+
+	auto func = [&](Box*& box, P2 pos2d)
+	{
+		P2 kvec = pos2d / L;
+		double k = len(kvec);
+		//???
+		double t = 0.0;
+
+		double wk = sqrt(g*k);
+
+		cplx h0_val = func_h0(func_Ph(kvec));
+
+		cplx h_val = h0_val * e_cplx(wk*t) + conju(func_h0(func_Ph(-kvec)))* e_cplx(-wk*t);
+
+		double height = 1 + h_val.real;
+
+		if (height < 0)
+		{
+			std::cout << height << " ";
+		}
+
+		box = new Box(P(pos2d.x, -2.5, pos2d.y), P(0.2, height, 0.2));
+		box->center.y += height/2;
+		op->PutShape(box, "oceanSpectum");
+	};
+	grid.DoByPos(func);
+	
+	auto screen = new rayTraceScreen(1080,720);
+	screen->Translate(P(0, 0, 7));
+	op->PutScreen(screen);
+	
+	auto light = new DirectionalLight(P(-1, -1, 0), P(1, 1, 1));
+	op->PutLight(light);
+
+	op->Evolve();
+
+	op->SaveScreenBufferFrame(screen,"color", pyWorkPath+"\\z.txt");
+	_chdir(pyWorkPath.data.c_str());
+	system("ZZZRUN_TransToImg.bat");
+	//op->SayI();
+	return 0;
+}
+#endif
